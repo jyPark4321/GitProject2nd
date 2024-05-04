01109ad927a5059e7c2dd7f32fb5c133e39eb6a4
jyPark4321 <qkrwnddus93@jbnu.ac.kr>
2024-05-04 13:48:23 +0900

    Initial commit

---
+++ src/main/groovy/com/example/springbootmvc/gitproject2nd/GitProject2ndApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.springbootmvc.gitproject2nd
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GitProject2ndApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(GitProject2ndApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/example/springbootmvc/gitproject2nd/GitProject2ndApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.springbootmvc.gitproject2nd
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GitProject2ndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
